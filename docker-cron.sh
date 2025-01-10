8d11637b6e1dfcdbe73b48d46248cc75ad30ac94
rogolezhou <176037470@qq.com>
2025-01-11 04:39:46 +0800

    每月1号自动续期ssl证书

--- docker-cron.sh
+++ docker-cron.sh
@@ -3,9 +3,9 @@
 set -euxo pipefail
 
 # 使用环境变量
-lego --email "$CLOUDFLARE_EMAIL" --dns cloudflare --domains "$DOMAIN" run
+lego --email "$CLOUDFLARE_EMAIL" --dns cloudflare --domains \"$WILDCARD_DOMAIN\" --domains "$DOMAIN" run
 
 # 添加续期任务到 crontab
-echo "0 0 * * * lego --email \"$CLOUDFLARE_EMAIL\" --dns cloudflare --domains \"$DOMAIN\" renew" | crontab -
+echo "0 6 1 * * lego --email \"$CLOUDFLARE_EMAIL\" --dns cloudflare --domains \"$WILDCARD_DOMAIN\" --domains \"$DOMAIN\" renew" | crontab -
 
 exec crond -n
